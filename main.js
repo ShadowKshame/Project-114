5d4ff76940330f2ffbe130aabe62a16b4bd74d66
ShadowKshame <104213448+ShadowKshame@users.noreply.github.com>
2022-12-14 20:31:06 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+mustacheX=0;
+mustacheY=0;
+
 function preload() {
+    mustache = loadImage('https://i.postimg.cc/g00FW22G/43-435215-mustache-silhouette-big-design-png-image-desenho-de.png');
 }
 
 function setup() {
@@ -17,8 +21,10 @@ function gotPoses(results)
     if (results.length > 0)
     {
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y = " + results[0].pose.nose.y);
+        mustacheX = results[0].pose.nose.x -15;
+        mustacheY = results[0].pose.nose.y;
+        console.log("mustache x = " + mustacheX);
+        console.log("mustache y = " + mustacheY);
     }
 }
 function modelLoaded() {
@@ -27,6 +33,10 @@ function modelLoaded() {
 
 function draw() {
     image(video,0,0,300,300);
+    fill(255,0,0);
+    stroke(255,0,0);
+    
+    image(mustache, mustacheX, mustacheY, 30, 30);
 }
 
 function take_snapshot(){
